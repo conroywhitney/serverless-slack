e55a1199bf84a673f9cfea9843e82b2aaa6248ed
Conroy Whitney <conroy.whitney@gmail.com>
2017-03-10 19:32:02 -0900

    Style thread/message list

--- app/src/renderer/components/Chat/ThreadSection.vue
+++ app/src/renderer/components/Chat/ThreadSection.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="thread-section">
-    <div class="thread-count">
-      <span v-show="unreadCount">
-        Unread threads: {{ unreadCount }}
-      </span>
-    </div>
     <ul class="thread-list">
       <thread
         v-for="thread in threads"
