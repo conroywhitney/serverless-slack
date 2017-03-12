aa5b9e713800d181314438668e0af678c6d98d1b
Conroy Whitney <conroy.whitney@gmail.com>
2017-03-12 14:19:43 -0800

    Flesh out `aws-iot-device-sdk` mock methods

--- test/mocks/aws-iot-device-sdk.js
+++ test/mocks/aws-iot-device-sdk.js
@@ -3,14 +3,26 @@ export default {
     console.log('mock', 'aws-iot-device-sdk', 'device')
 
     return {
+      onClose: null,
+      onConnect: null,
+      onMessage: null,
+
       end () {
         console.log('mock', 'aws-iot-device-sdk', 'device', 'end')
+
+        this.onClose()
       },
       on (event, cb) {
         console.log('mock', 'aws-iot-device-sdk', 'device', 'on', event)
+
+        if (event === 'close') this.onClose = cb
+        if (event === 'connect') this.onConnect = cb
+        if (event === 'message') this.onMessage = cb
       },
-      publish () {
-        console.log('mock', 'aws-iot-device-sdk', 'device', 'publish')
+      publish (message) {
+        console.log('mock', 'aws-iot-device-sdk', 'device', 'publish', message)
+
+        this.onMessage(message)
       },
       subscribe () {
         console.log('mock', 'aws-iot-device-sdk', 'device', 'subscribe')
