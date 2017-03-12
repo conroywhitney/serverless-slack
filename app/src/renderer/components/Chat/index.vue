ac3a9e134d23679d2720e66e9bb49a7ea38eef83
Conroy Whitney <conroy.whitney@gmail.com>
2017-03-12 15:09:43 -0800

    Move `components/Chat.vue` into `components/Chat/index.vue`
    Also move `css/chat.css` into `components/Chat/styles.css`

---
+++ app/src/renderer/components/Chat/index.vue
@@ -0,0 +1,27 @@
+<script>
+import ThreadSection from './ThreadSection.vue'
+import MessageSection from './MessageSection.vue'
+
+export default {
+  name: 'Chat',
+  components: {
+    ThreadSection,
+    MessageSection
+  },
+  mounted: function () {
+    this.$store.dispatch('iotConnect')
+  },
+  destroyed: function () {
+    this.$store.dispatch('iotDisconnect')
+  }
+}
+</script>
+
+<style src="./styles.css"></style>
+
+<template>
+  <div class="chatapp">
+    <thread-section></thread-section>
+    <message-section></message-section>
+  </div>
+</template>
