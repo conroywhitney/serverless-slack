948d8d0661fbd74497ee78c6a208d43197ace2e5
Conroy Whitney <conroy.whitney@gmail.com>
2017-03-14 00:27:22 -0800

    Extend message with `isRead` attribute

--- app/src/renderer/store/chat/mutations.js
+++ app/src/renderer/store/chat/mutations.js
@@ -40,15 +40,19 @@ function createThread (state, id, name) {
 
 function addMessage (state, message) {
   // add a `isRead` field before adding the message
-  message.isRead = message.threadID === state.currentThreadID
+  const newMessage = {
+    ...message,
+    isRead: message.threadID === state.currentThreadID
+  }
+
   // add it to the thread it belongs to
-  const thread = state.threads[message.threadID]
-  if (!thread.messages.some(id => id === message.id)) {
-    thread.messages.push(message.id)
-    thread.lastMessage = message
+  const thread = state.threads[newMessage.threadID]
+  if (!thread.messages.some(id => id === newMessage.id)) {
+    thread.messages.push(newMessage.id)
+    thread.lastMessage = newMessage
   }
   // add it to the messages map
-  Vue.set(state.messages, message.id, message)
+  Vue.set(state.messages, newMessage.id, newMessage)
 }
 
 function setCurrentThread (state, id) {
